3864c1a294f1b2abe0b4baadf87d11671a98f0fa
ameeralic <ameeralic95@gmail.com>
2022-11-16 01:44:05 +0530

    second  commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('uploads'))
 
 //database connection
-mongoose.connect(process.env.DB_URI,{
+mongoose.connect(process.env.DB_URI_LIVE,{
     useNewUrlParser : true,
     useUnifiedTopology: true
     //useFindAndModify: true,
